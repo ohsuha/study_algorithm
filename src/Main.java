6b90b4f38dd9ff1fefdc29a9609a412883257c72
sue_오수하 <shoh@maxst.com>
2024-08-04 20:24:36 +0900

    [algorithm_study_M] 기본 설정

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src;
+
+import src.algorithm.Study_01;
+
+public class Main {
+    public static void main(String[] args) {
+        Study_01 study_01 = new Study_01();
+        study_01.algorithm();
+    }
+}
