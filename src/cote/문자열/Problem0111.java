ca1fb6f0535e019af00144b797fb837b9aaa8d5c
sue <shoh@maxst.com>
2024-09-03 18:00:05 +0900

    cote

--- src/cote/문자열/Problem0111.java
+++ src/cote/문자열/Problem0111.java
@@ -20,11 +20,32 @@ public class Problem0111 {
 			if (count != 1) {
 				result += count + "" + str[i];
 				count = 1;
-			} else{
+			} else {
 				result += str[i];
 				count = 1;
 			}
 		}
 		System.out.println(result);
+	}
+
+	public void solution() {
+		Scanner scanner = new Scanner(System.in);
+		String str = scanner.nextLine();
+		String answer = "";
+		str = str + " "; //마지막 문자, 공백과 비교하기 위해
+		int cnt = 1;
+
+		for (int i = 0; i < str.length() - 1; i++) {
+			if (str.charAt(i) == str.charAt(i + 1)) {
+				cnt++;
+			} else {
+				answer += str.charAt(i);
+				if (cnt > 1) {
+					answer += String.valueOf(cnt);
+				}
+				cnt = 1;
+			}
+		}
+		System.out.println(answer);
 	}
 }
