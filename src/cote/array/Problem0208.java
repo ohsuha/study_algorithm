7dedb332d6d2067057070c982d2de794e673d595
sue <shoh@maxst.com>
2024-09-09 17:35:06 +0900

    cote05

--- src/cote/array/Problem0208.java
+++ src/cote/array/Problem0208.java
@@ -27,4 +27,24 @@ public class Problem0208 {
 			System.out.print(result[i] + " ");
 		}
 	}
+
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		int[] answer = new int[n];
+		for (int i = 0; i < n; i++) {
+			int cnt = 1;
+			for (int j = 0; j < n; j++) {
+				if(arr[j]>arr[i]){
+					cnt++;
+				}
+			}
+			answer[i] = cnt;
+		}
+	}
 }
--- src/cote/array/Problem0209.java
+++ src/cote/array/Problem0209.java
@@ -70,4 +70,38 @@ public class Problem0209 {
 		System.out.println(max);
 	}
 
+	public void solution() {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n][n];
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+
+		int answer = Integer.MAX_VALUE;
+
+		int sum1;
+		int sum2;
+
+		for (int i = 0; i < n; i++) {
+			sum1 = sum2 = 0;
+			for (int j = 0; j < n; j++) {
+				sum1 += arr[i][j];
+				sum2 += arr[j][i];
+			}
+			answer = Math.max(answer, sum1);
+			answer = Math.max(answer, sum2);
+		}
+
+		sum1=sum2=0;
+		for (int i = 0; i < n; i++) {
+			sum1 += arr[i][i];
+			sum2+=arr[i][n-i-1];
+		}
+
+	}
+
 }
