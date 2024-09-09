6b1b2785af0737b1032946a24165d1a7b9eaad54
sue <shoh@maxst.com>
2024-09-09 12:05:50 +0900

    cote05

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ import src.cote.array.Problem0205;
 import src.cote.array.Problem0206;
 import src.cote.array.Problem0207;
 import src.cote.array.Problem0208;
+import src.cote.array.Problem0209;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0208 pro = new Problem0208();
+		Problem0209 pro = new Problem0209();
 		pro.algorithm();
 	}
 }
