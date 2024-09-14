ca26e086fe0b5bf7dbe968493776327a3c632a2d
suhaoh <suha.oh91@gmail.com>
2024-09-14 16:56:38 +0900

    cote_08

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 package src;
 
 import src.cote.twopointers.Problem0301;
+import src.cote.twopointers.Problem0302;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0301 pro = new Problem0301();
-		pro.solution();
+		Problem0302 pro = new Problem0302();
+		pro.algorithm();
 	}
 }
