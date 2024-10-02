b934b4f8e812d8f06cc465c8b7a5177eb147a782
suhaoh <suha.oh91@gmail.com>
2024-10-02 13:21:17 +0900

    cote_12

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,11 @@ package src;
 import src.cote.hash.Problem0404;
 import src.cote.hash.Problem0405;
 import src.cote.stack_queue.Problem0501;
+import src.cote.stack_queue.Problem0502;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0501 pro = new Problem0501();
+		Problem0502 pro = new Problem0502();
 		pro.algorithm();
 	}
 }
--- src/cote/stack_queue/Problem0502.java
+++ src/cote/stack_queue/Problem0502.java
@@ -1,4 +1,31 @@
 package src.cote.stack_queue;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Problem0502 {
+	public void algorithm() {
+		Scanner scanner = new Scanner(System.in);
+		String input = scanner.nextLine();
+		String[] strings = input.split("");
+
+		int count = 0;
+		String answer = "";
+
+		for (int i = 0; i < strings.length; i++) {
+			if (strings[i].equals("(")) {
+				count++;
+				continue;
+			} else if (strings[i].equals(")")) {
+				count--;
+				continue;
+			}
+
+			if (count == 0) {
+				answer += strings[i];
+			}
+		}
+
+		System.out.println(answer);
+	}
 }
