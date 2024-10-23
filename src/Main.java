5feba0fa698a86bc8029588a7f06db78681cfa30
suhaoh <suha.oh91@gmail.com>
2024-10-23 12:10:04 +0900

    factorial

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ package src;
 
 import src.cote.DFS_BFS_basic.Problem0701;
 import src.cote.DFS_BFS_basic.Problem0702;
+import src.cote.DFS_BFS_basic.Problem0703;
 import src.cote.정렬.Problem0607;
 import src.cote.정렬.Problem0608;
 import src.cote.정렬.Problem0609;
@@ -9,7 +10,7 @@ import src.cote.정렬.Problem0610;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0702 pro = new Problem0702();
+		Problem0703 pro = new Problem0703();
 		pro.solution();
 	}
 }
