027316d426a104c3ebacf3c0a5818b4289a2243d
suhaoh <suha.oh91@gmail.com>
2024-10-23 12:33:20 +0900

    fibonacci

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ package src;
 import src.cote.DFS_BFS_basic.Problem0701;
 import src.cote.DFS_BFS_basic.Problem0702;
 import src.cote.DFS_BFS_basic.Problem0703;
+import src.cote.DFS_BFS_basic.Problem0704;
 import src.cote.정렬.Problem0607;
 import src.cote.정렬.Problem0608;
 import src.cote.정렬.Problem0609;
@@ -10,7 +11,7 @@ import src.cote.정렬.Problem0610;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0703 pro = new Problem0703();
+		Problem0704 pro = new Problem0704();
 		pro.solution();
 	}
 }
