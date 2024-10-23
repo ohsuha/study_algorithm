943b0bb35dd8267b197d56c3c90c41ae1da6a758
suhaoh <suha.oh91@gmail.com>
2024-10-23 22:45:27 +0900

    DFS

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import src.cote.DFS_BFS_basic.Problem0701;
 import src.cote.DFS_BFS_basic.Problem0702;
 import src.cote.DFS_BFS_basic.Problem0703;
 import src.cote.DFS_BFS_basic.Problem0704;
+import src.cote.DFS_BFS_basic.Problem0705;
 import src.cote.정렬.Problem0607;
 import src.cote.정렬.Problem0608;
 import src.cote.정렬.Problem0609;
@@ -11,7 +12,7 @@ import src.cote.정렬.Problem0610;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0704 pro = new Problem0704();
+		Problem0705 pro = new Problem0705();
 		pro.solution();
 	}
 }
---
+++ src/cote/DFS_BFS_basic/Problem0705.java
@@ -0,0 +1,44 @@
+package src.cote.DFS_BFS_basic;
+
+public class Problem0705 {
+	//이진 트리 순회 (깊이 우선 탐색)
+	// 전위 순회 출력 : 1 2 4 5 3 6 7 부모를 제일 먼저 방문 -> 왼쪽 -> 오른쪽
+	// 중위 순회 : 4 2 5 1 6 3 7 가장 왼쪽 -> 부모 -> 오른쪽
+	// 후위 순회 : 4 5 2 6 7 3 1  왼 -> 오른 -> 부모
+	// 방문 = 출력
+	// 깊이 우선 탐색은 계속 부모에서 타고 타고 내려감
+
+	Node root;
+
+	public void DFS(Node root) {
+		if (root == null) {
+			return;
+		} else {
+			// System.out.println(root.data); // 여기서 출력을 찍으면 전위 순회
+			DFS(root.lt);
+			System.out.println(root.data); // 여기서 출력을 찍으면 중위 순회
+			DFS(root.rt);
+			// System.out.println(root.data); // 여기서 출력을 찍으면 후위 순회
+		}
+	}
+
+	public void solution() {
+		Problem0705 tree = new Problem0705();
+		tree.root = new Node(1);
+		tree.root.lt = new Node(2);
+		tree.root.rt = new Node(3);
+		tree.root.lt.lt = new Node(4);
+		tree.root.lt.rt = new Node(5);
+		tree.DFS(tree.root);
+	}
+
+	public class Node {
+		int data;
+		Node lt, rt;
+
+		public Node(int data) {
+			this.data = data;
+			lt = rt = null;
+		}
+	}
+}
