86d90c12ac1d319287a5e5d237c436a25f63f07f
suhaoh <suha.oh91@gmail.com>
2024-10-23 23:58:42 +0900

    BFS

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import src.cote.DFS_BFS_basic.Problem0703;
 import src.cote.DFS_BFS_basic.Problem0704;
 import src.cote.DFS_BFS_basic.Problem0705;
 import src.cote.DFS_BFS_basic.Problem0706;
+import src.cote.DFS_BFS_basic.Problem0707;
 import src.cote.정렬.Problem0607;
 import src.cote.정렬.Problem0608;
 import src.cote.정렬.Problem0609;
@@ -13,7 +14,7 @@ import src.cote.정렬.Problem0610;
 
 public class Main {
 	public static void main(String[] args) {
-		Problem0706 pro = new Problem0706();
+		Problem0707 pro = new Problem0707();
 		pro.solution();
 	}
 }
--- src/cote/DFS_BFS_basic/Problem0705.java
+++ src/cote/DFS_BFS_basic/Problem0705.java
@@ -1,14 +1,14 @@
 package src.cote.DFS_BFS_basic;
 
 public class Problem0705 {
-	//이진 트리 순회 (깊이 우선 탐색)
+	// 이진 트리 순회 (깊이 우선 탐색)
 	// 전위 순회 출력 : 1 2 4 5 3 6 7 부모를 제일 먼저 방문 -> 왼쪽 -> 오른쪽
 	// 중위 순회 : 4 2 5 1 6 3 7 가장 왼쪽 -> 부모 -> 오른쪽
 	// 후위 순회 : 4 5 2 6 7 3 1  왼 -> 오른 -> 부모
 	// 방문 = 출력
 	// 깊이 우선 탐색은 계속 부모에서 타고 타고 내려감
 
-	Node root;
+	private Node root;
 
 	public void DFS(Node root) {
 		if (root == null) {
